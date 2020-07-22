80655543c557287283e855f055dab6f85d66f1be
enes2101 <48836015+enes2101@users.noreply.github.com>
2020-07-22 18:54:25 +0300

    1.gün

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    
+    public float speed;
+    
+    void Update()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = transform.TransformDirection(transform.right) * moveHorizontal;
+
+        transform.position += movement * Time.deltaTime * speed;
+
+        if (moveHorizontal>0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
