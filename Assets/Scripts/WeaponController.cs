606ae3c9d0fbd8e7379991ce2c347aa758d3e623
enes2101 <48836015+enes2101@users.noreply.github.com>
2020-07-25 10:32:37 +0300

    3. gün

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public bool onAttack=false;
+
+    void Update()
+    {
+        //C ye basılırsa rengi değişsin
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            StartCoroutine(WaitAndChangeColor());
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0)&&onAttack==false)
+        {
+            onAttack = true;
+            transform.rotation = Quaternion.Euler(0, 0, 45);
+            StartCoroutine(WaitForAttack());
+        }
+
+    }
+
+    IEnumerator WaitAndChangeColor()
+    {
+        yield return new WaitForSeconds(2);
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+    IEnumerator WaitForAttack()
+    {
+        yield return new WaitForSeconds(1);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        onAttack = false;
+    }
+
+}
